733765712195b30edd27dc4faff2a10519a8a3cf
Dan Kim <danchanyoungkim@gmail.com>
2022-06-22 16:23:51 -0400

    Add minimal changes into href/src

--- src/components/Bear/Bear.js
+++ src/components/Bear/Bear.js
@@ -10,10 +10,11 @@ function Bear() {
                 <h2 className="reminderH2"><p className="reminderP">Reminder</p></h2>
             </div>
             <div className="speechRow">
-                <h2 className="speechColumn Left"><img alt="" className="code" src={code}></img>Do not forget to RVSP by July 9th!</h2>
-                {/* <h2 className="speechColumn Mid">?asdas</h2> */}
+                    <h2 className="speechColumn Left"><a href="https://www.zola.com/wedding/royandsharon2022"><img alt="" className="code" src={code}></img></a>Do not forget to RVSP by July 9th!</h2>
             </div>
-            <img className="bear" src={bear} alt=""/>
+            <a href="https://github.com/danchanyoungkim">
+                <img className="bear" src={bear} alt=""/>
+            </a>
         </div>
     )
 }
